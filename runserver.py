61442b6acc06755715bb5c25b789e64c8e46ffd5
Markus Baden <markus.baden@gmail.com>
2013-08-17 10:33:24 +0800

    Changed port to not conflict with IPython notebook

--- runserver.py
+++ runserver.py
@@ -3,22 +3,23 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.web
 
+
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print "Websocket opened"
 
     def on_message(self, message):
         self.write_message("You said: " + message)
-    
+
     def on_close(self):
         print "WebSocket closed"
 
 application = tornado.web.Application([
     (r'/ws', WSHandler),
 ])
- 
- 
+
+
 if __name__ == "__main__":
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8888)
+    http_server.listen(5050)
     tornado.ioloop.IOLoop.instance().start()
